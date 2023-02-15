5b30a020a8cd83dd97b009e8026d2775aca93946
Bhaveshkadam <bhaukadam508@gmail.com>
2023-02-15 15:23:56 +0530

    added UserController

---
+++ src/main/java/com/passbook/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.passbook.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+public class UserController {
+
+}
