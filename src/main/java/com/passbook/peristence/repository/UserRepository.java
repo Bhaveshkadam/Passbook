071cf55a4c7ae1acc6ab8ddb5b881e8b302ad34e
Bhaveshkadam <bhaukadam508@gmail.com>
2023-02-15 15:21:14 +0530

    repository

---
+++ src/main/java/com/passbook/peristence/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.passbook.peristence.repository;
+
+import com.passbook.peristence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository  extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
+}
