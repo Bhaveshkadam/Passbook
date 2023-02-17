3155746209d98e071e4b29f585cf5fe66217fde2
Bhaveshkadam <bhaukadam508@gmail.com>
2023-02-17 15:44:51 +0530

    added code

--- src/main/java/com/passbook/service/UserService.java
+++ src/main/java/com/passbook/service/UserService.java
@@ -1,8 +1,64 @@
 package com.passbook.service;
 
+import com.passbook.peristence.POJO.LoginRequest;
+import com.passbook.peristence.POJO.LoginResponse;
+import com.passbook.peristence.POJO.SignUpRequest;
+import com.passbook.peristence.POJO.SignUpResponse;
+import com.passbook.peristence.entity.UserEntity;
+import com.passbook.peristence.repository.UserRepository;
+import lombok.NonNull;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public SignUpResponse signUp(@NonNull final SignUpRequest signUpRequest) throws Exception {
+
+        final Optional<UserEntity> user =
+                userRepository.findByEmail(signUpRequest.getEmail());
+
+        if (user.isPresent()) {
+            return SignUpResponse.builder()
+                    .message("Email already exists.")
+                    .build();
+        }
+
+        userRepository.save(UserEntity.builder()
+                .email(signUpRequest.getEmail())
+                .password(signUpRequest.getPassword())
+                .build());
+
+        return SignUpResponse.builder()
+                .message("Sign-up is successful.")
+                .build();
+    }
+
+    public LoginResponse login(final LoginRequest userLogin) throws Exception {
+        Optional<UserEntity> email = userRepository
+                .findByEmail(userLogin.getEmail());
+        if (!email.isPresent()) {
+            return LoginResponse.builder()
+                    .message("User not registered with provided email")
+                    .build();
+
+        } else if (email.isPresent()) {
+            UserEntity userEntity = email.get();
+            if (!userEntity.getPassword().equals(userLogin.getPassword())) {
+                return LoginResponse.builder()
+                        .message("Enter valid password to continue")
+                        .build();
+            }
+        }
+        return LoginResponse.builder()
+                .message("User login successful")
+                .build();
+    }
 
 }
